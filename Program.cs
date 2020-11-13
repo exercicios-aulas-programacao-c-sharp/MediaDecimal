22cc0537781f741529a75b6d37cd36da03afc1e9
Diego Neri <diego@diegoneri.com>
2020-11-13 19:11:02 -0300

    Cria novo projeto dotnet console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MediaDecimal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
